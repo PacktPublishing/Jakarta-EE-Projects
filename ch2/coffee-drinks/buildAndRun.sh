9e2be1baa24a5d53179dceee48dec958776e9b00
prashantp <prashant.pro@gmail.com>
2019-03-15 15:11:50 +0530

    port binding changed in buildAndRun.sh

--- ch2/coffee-drinks/buildAndRun.sh
+++ ch2/coffee-drinks/buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mvn clean package && docker build -t prashantpro/coffee-drinks .
-docker rm -f coffee-drinks || true && docker run -d -p 8080:8080 --name coffee-drinks prashantpro/coffee-drinks
+docker rm -f coffee-drinks || true && docker run -d -p 8020:8080 --name coffee-drinks prashantpro/coffee-drinks
